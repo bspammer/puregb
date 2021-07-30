782be7ead042f3ebc20e1490ef74a67644bcbca3
Charlie Collard <charlie.collard@marshmallow.co>
2021-07-30 22:47:40 +0100

    Rom: implement title and licensee extraction

---
+++ src/Rom/Rom.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Rom.Rom where
+
+import Data.Map (lookup)
+import Data.ByteString as B (ByteString, readFile, index, splitAt, pack, break)
+import Data.ByteString.UTF8 as UTF8 (toString)
+import Path.Posix (Path, Abs, File, fromAbsFile, mkAbsFile)
+import Data.Word (Word8, Word16)
+import Data.FileEmbed (embedFile)
+import Test.QuickCheck
+import Test.QuickCheck.All (quickCheckAll)
+import Test.QuickCheck.Gen (elements)
+import Rom.Enum (LicenseeCode(..), licenseeMap)
+
+data Rom = Rom {
+    title :: String,
+    licenseeCode :: LicenseeCode,
+    sGBFlag :: Word8,
+    cartridgeType :: Word8,
+    romSize :: Word8,
+    ramSize :: Word8,
+    destinationCode :: Word8,
+    versionNumber :: Word8,
+    headerChecksum :: Word8,
+    globalChecksum :: Word16
+}
+
+exampleRom = $(embedFile "data/tobutobugirl/tobu.gb")
+
+rangeFromByteString :: ByteString -> Int -> Int -> ByteString
+rangeFromByteString byteString low high = result
+    where
+        (_, right) = B.splitAt low byteString
+        (result, _) = B.splitAt (high - low) right
+
+
+readRom :: FilePath -> IO Rom
+readRom filepath = do
+    rawBytes <- B.readFile filepath
+    return Rom {
+        title = extractTitle rawBytes,
+        licenseeCode = extractLicenseeCode rawBytes
+    }
+
+
+prop_extractTitle = extractTitle exampleRom == "TOBU"
+extractTitle :: ByteString -> String
+extractTitle rom = UTF8.toString result
+    where 
+        titleStartByte = 0x134
+        titleEndByte = 0x143
+        rawBytes = rangeFromByteString rom titleStartByte titleEndByte
+        (result, _) = B.break (== 0) rawBytes
+
+prop_extractLicenseeCode = extractLicenseeCode exampleRom == None
+extractLicenseeCode :: ByteString -> LicenseeCode
+extractLicenseeCode rom = case maybeCode of
+    (Just code) -> code
+    Nothing -> None
+    where
+        licenseeByte = 0x144
+        maybeCode = Data.Map.lookup (B.index rom licenseeByte) licenseeMap
+
+
+return []
+runTests = $quickCheckAll
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,17 @@
+import Paths_puregb
 import Tile
+import Rom.Rom
+import Data.ByteString as B (readFile)
 import System.Exit (exitSuccess, exitFailure)
 import Control.Monad (sequence)
-
 
 main :: IO ()
 main = do
     putStrLn "Running tests"
-    results <- sequence [Tile.runTests]
+    results <- sequence [
+      -- Tile.runTests,
+      Rom.Rom.runTests
+      ]
     if and results
       then exitSuccess
       else exitFailure
