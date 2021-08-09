daddcde8bc4294a38cd8b6b395a589b5eef08176
Charlie Collard <charlie.collard@marshmallow.co>
2021-08-09 22:00:18 +0100

    Implement flag getters

--- src/CPU.hs
+++ src/CPU.hs
@@ -15,11 +15,28 @@ data CPU = CPU {
     hl :: Register
 }
 
+data Flag = Zero | Subtraction | HalfCarry | Carry
+
 prop_splitRegister0 b1 b2 = splitRegister (Register (shiftL (fromIntegral b1) 8 .|. fromIntegral b2)) == (SubRegister b1, SubRegister b2)
 prop_splitRegister1 = splitRegister (Register 0xffff) == (SubRegister 0xff, SubRegister 0xff)
 prop_splitRegister2 = splitRegister (Register 0xabcd) == (SubRegister 0xab, SubRegister 0xcd)
 splitRegister :: Register -> (SubRegister, SubRegister)
 splitRegister (Register word) = (SubRegister $ fromIntegral (shiftR (word .&. 0xff00) 8), SubRegister $ fromIntegral (word .&. 0xff))
+
+bitForFlag :: Flag -> Int
+bitForFlag Zero = 7
+bitForFlag Subtraction = 6
+bitForFlag HalfCarry = 5
+bitForFlag Carry = 4
+
+prop_getFlagZero byte = getFlag Zero CPU {flags = SubRegister byte} == (byte .&. 0x80 > 0)
+prop_getFlagSubtraction byte = getFlag Subtraction CPU {flags = SubRegister byte} == (byte .&. 0x40 > 0)
+prop_getFlagHalfCarry byte = getFlag HalfCarry CPU {flags = SubRegister byte} == (byte .&. 0x20 > 0)
+prop_getFlagCarry byte = getFlag Carry CPU {flags = SubRegister byte} == (byte .&. 0x10 > 0)
+getFlag :: Flag -> CPU -> Bool
+getFlag flag CPU {flags = SubRegister f} = extractBit (bitForFlag flag) > 0
+    where
+        extractBit bit = f .&. shiftL 1 bit
 
 return []
 runTests = $quickCheckAll
